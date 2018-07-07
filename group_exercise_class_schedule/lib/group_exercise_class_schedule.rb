ce8719ac71a2e25f86b1793800d3b54dc9776a5a
Michael Blandford <mdblandford@gmail.com>
2018-07-07 16:04:02 +0000

    automatically backed up by learn

--- group_exercise_class_schedule/lib/group_exercise_class_schedule.rb
+++ group_exercise_class_schedule/lib/group_exercise_class_schedule.rb
@@ -1,6 +1,2 @@
 require_relative "./group_exercise_class_schedule/version"
 require_relative "./group_exercise_class_schedule/cli"
-
-module GroupExerciseClassSchedule
-  # Your code goes here...
-end
